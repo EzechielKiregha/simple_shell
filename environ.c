27661523e8605d9a5c43ec135389a1157a0c715b
Collinzy0451 <tamunosakicollinsjacob@gmail.com>
2023-12-05 11:47:08 -0800

    Built-in commands to manage environment variables

--- environ.c
+++ environ.c
@@ -48,5 +48,48 @@ int env_command(ProgramData *data)
 	}
 	return (0);
 }
+/**
+ * set_env - Set or update an environment variable.
+ * @data: Pointer to the ProgramData structure.
+ * Return: 0 on success,5 if too many arguments,or as specified in arguments.
+ */
+int set_env(ProgramData *data)
+{
+	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
+		return (0);
 
+	/Check for too many arguments/
+		if (data->tokens[3] != NULL)
+		{
+			errno = E2BIG;
+			perror(data->command_name);
+			return (5);
+		}
+	/Set or update the environment variable/
+		env_set_key(data->tokens[1], data->tokens[2], data);
 
+	return (0);
+}
+
+/**
+ * unset_env - Unset or remove an environment variable.
+ * @data: Pointer to the ProgramData structure.
+ * Return: 0 on success, 5 if too many arguments, or as specified in arguments.
+ */
+int unset_env(ProgramData *data)
+{
+	if (data->tokens[1] == NULL)
+		return (0);
+
+	/Check for too many arguments/
+		if (data->tokens[2] != NULL)
+		{
+			errno = E2BIG;
+			perror(data->command_name);
+			return (5);
+		}
+	/Unset or remove the environment variable/
+		env_remove_key(data->tokens[1], data);
+
+	return (0);
+}
