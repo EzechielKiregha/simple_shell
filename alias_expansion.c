b646b9ca22167363ce13329343715f62555dfd28
Collinzy0451 <tamunosakicollinsjacob@gmail.com>
2023-12-07 14:56:24 -0800

    Expands aliases in the input line

--- alias_expansion.c
+++ alias_expansion.c
@@ -70,3 +70,50 @@ int add_to_buffer(char *buffer, char *str_to_add)
 
 	return (length + i); /*the new length of the buffer is returned*/
 }
+/**
+ * expand_alias - Expand aliases in the input line.
+ * @data: Pointer to the ProgramData structure.
+ * Return: None.
+ */
+void expand_alias(ProgramData *data)
+{
+	int i, j, expanded = 0;
+	char line[BUFFER_SIZE] = {0}, var_expand[BUFFER_SIZE] = {'\0'}, *temp;
+
+	if (data->input_line == NULL)
+		return;
+
+	add_to_buffer(line, data->input_line);
+
+	for (i = 0; line[i]; i++)
+	{
+		/* Extract the potential alias name */
+		for (j = 0; line[i + j] && line[i + j] != ' '; j++)
+			var_expand[j] = line[i + j];
+		var_expand[j] = '\0';
+
+		/* Get the value of the alias from the alias list */
+		temp = alias_get(data, var_expand);
+
+		/* If the alias is found, expand it in the line */
+		if (temp)
+		{
+			/* Update the line with the expanded alias */
+			var_expand[0] = '\0';
+			add_to_buffer(var_expand, line + i + j);
+			line[i] = '\0';
+			add_to_buffer(line, temp);
+			line[str_length(line)] = '\0';
+			add_to_buffer(line, var_expand);
+			expanded = 1;
+		}
+		break; / Stop after the first alias var_expand /
+	}
+
+	/* Check if the input line has changed and update it */
+	if (expanded)
+	{
+		free(data->input_line);
+		data->input_line = str_duplicate(line);
+	}
+}
